d83b6eaeb5f75825ee4b48bb0f2d82da2d8481d0
zabackary <137591653+zabackary@users.noreply.github.com>
2024-11-03 17:22:15 +0900

    feat: lock the instance after usage

--- supabase/functions/insert-take/index.ts
+++ supabase/functions/insert-take/index.ts
@@ -23,6 +23,22 @@ Deno.serve(async (req) => {
     return new Response("need instanceId parameter", { status: 400 });
   }
 
+  // lock the instance, if needed
+  const { data: instance, error: instanceError } = await supabase.from(
+    "instance",
+  ).select("paid_is_unlocked").eq("id", instanceId).single();
+  if (instanceError) {
+    return new Response(JSON.stringify(instanceError), { status: 400 });
+  }
+  if (instance.paid_is_unlocked !== null) {
+    const { error } = await supabase.from("instance").update({
+      paid_is_unlocked: false,
+    }).eq("id", instanceId).single();
+    if (error) {
+      return new Response(JSON.stringify(error), { status: 400 });
+    }
+  }
+
   const update = await supabase.from("take").insert({
     processed_url: processedUrl,
     raw_urls: rawUrls,
